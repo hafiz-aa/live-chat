2f75c9f1338d4fdf94c9c337cd0435f06a3cdb5a
hafiz-aa <mhafiz.aa@gmail.com>
2022-06-03 13:52:29 +0700

    commit on step-02

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhdZ1IAp073tMP0ubclRjxCXlq86TLXvw",
+  authDomain: "hafiz-blog.firebaseapp.com",
+  projectId: "hafiz-blog",
+  storageBucket: "hafiz-blog.appspot.com",
+  messagingSenderId: "840706888039",
+  appId: "1:840706888039:web:8a35776cc219c08bf65fc2"
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
