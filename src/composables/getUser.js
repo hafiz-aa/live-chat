1a1b4fbded3f32d6ff16776ddfda58361b7b7023
hafiz-aa <mhafiz.aa@gmail.com>
2022-06-09 14:35:12 +0700

    commit on watching current user

--- src/composables/getUser.js
+++ src/composables/getUser.js
@@ -2,13 +2,14 @@ import { projectAuth } from "@/firebase/config";
 import { ref } from "vue";
 
 const user = ref(projectAuth.currentUser)
+
 projectAuth.onAuthStateChanged(_user => {
   console.log('User state change. Current user is: ', _user)
   user.value = _user
 })
 
 const getUser = () => {
-  return {user}
+  return { user }
 }
 
 export default getUser
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -7,9 +7,22 @@
 
 <script>
 import Navbar from '../components/Navbar'
+import getUser from '../composables/getUser'
+import { useRouter } from 'vue-router'
+import { watch } from 'vue'
 
 export default {
-  components: { Navbar }
+  components: { Navbar },
+  setup() {
+    const router = useRouter()
+    const { user } = getUser()
+
+    watch(user, () => {
+      if (!user.value)  {
+        router.push({ name: 'Welcome'})
+      }
+    })
+  }
 }
 </script>
 
